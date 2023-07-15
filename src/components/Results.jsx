cb11713f9af424b191b8ce476897a1fcca2f58a6
Timothy <tim87henry@gmail.com>
2023-07-15 19:43:27 +1000

    Created the results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Results = () => {
   return (
-    <div className='result'>Results</div>
+    <div className='result'>
+      <h2>Your Result</h2>
+      <div className="marks">
+        <h1>76</h1>
+        <p>of 100</p>
+      </div>
+      <div className="remarks">
+        <h2>Great</h2>
+        <p>You scored higher than 65% of the people who have taken these tests.</p>
+      </div>
+    </div>
   )
 }
 
