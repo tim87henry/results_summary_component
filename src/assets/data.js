dad11af369f1e7500ce52eeb5a9431d68e3546ba
Timothy <tim87henry@gmail.com>
2023-08-03 23:41:37 +1000

    Completed desktop design

---
+++ src/assets/data.js
@@ -0,0 +1,33 @@
+import icon1 from "../assets/images/icon-reaction.svg";
+import icon2 from "../assets/images/icon-memory.svg";
+import icon3 from "../assets/images/icon-verbal.svg";
+import icon4 from "../assets/images/icon-visual.svg";
+
+const data = [
+  {
+    "category": "Reaction",
+    "score": 80,
+    "icon": icon1,
+    "classname": "red"
+  },
+  {
+    "category": "Memory",
+    "score": 92,
+    "icon": icon2,
+    "classname": "yellow"
+  },
+  {
+    "category": "Verbal",
+    "score": 61,
+    "icon": icon3,
+    "classname": "green"
+  },
+  {
+    "category": "Visual",
+    "score": 72,
+    "icon": icon4,
+    "classname": "blue"
+  }
+]
+
+export default data;
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import data from '../assets/data';
+
+const Subject = (props) => {
+  let classes = "subject "+props.item.classname; 
+  return (
+    <div className={classes}>
+      <div>
+        <img src={props.item.icon} alt=""></img>
+        <h3>{props.item.category}</h3>
+      </div>
+      <p><b>{props.item.score}</b> / 100</p>
+    </div>
+  )
+}
 
 const Summary = () => {
   return (
-    <div className='summary'>Summary</div>
+    <div className='summary'>
+      <h2>Summary</h2>
+      {data.map((item) =>
+        <Subject item={item} />)
+      }
+      <button className='continue'><b>Continue</b></button>
+    </div>
   )
 }
 
